59d475c5aa63c07f66ba9d0b84a12884ae6f79ff
Danyel Pinheiro <danyelpinheiro@gmail.com>
2026-01-06 19:26:48 -0300

    Initial commit

---
+++ EstacionaBenner/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace EstacionaBenner.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
